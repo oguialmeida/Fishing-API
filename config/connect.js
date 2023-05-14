dc74d53b6d73fb4c3263e41c342397082b0a036a
Guilherme Gomes <gui.rock08@gmail.com>
2023-05-14 02:43:56 -0300

    Update

---
+++ config/connect.js
@@ -0,0 +1,17 @@
+const { OPEN_CREATE } = require("sqlite3");
+
+const sqlite3 = require("sqlite3").verbose();
+
+let db = new sqlite3.Database("../data/datafish.db", sqlite3.OPEN_READWRITE | OPEN_CREATE, (err) => {
+    if(err) {
+        return console.error(err.message);
+    };
+    console.log("Connected to the in-memory database");
+});
+
+// db.close((err) =>{
+//     if (err) {
+//         return console.error(err.message);
+//     }
+//     console.log("Close the connection");
+// });
