025b82d04d59d0dd883b8bf74b9ca382a42eb6a1
Guilherme Gomes <gui.rock08@gmail.com>
2023-05-15 17:51:07 -0300

    Update

--- config/connect.js
+++ config/connect.js
@@ -1,17 +1,7 @@
-const { OPEN_CREATE } = require("sqlite3");
-
-const sqlite3 = require("sqlite3").verbose();
-
-let db = new sqlite3.Database("../data/datafish.db", sqlite3.OPEN_READWRITE | OPEN_CREATE, (err) => {
-    if(err) {
-        return console.error(err.message);
-    };
-    console.log("Connected to the in-memory database");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "../data/datafish.db",
 });
 
-db.close((err) =>{
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log("Close the connection");
-});
+module.exports = sequelize;
---
+++ model/fisher.js
@@ -0,0 +1,37 @@
+const Sequelize = require("sequelize");
+const db = require("../config/connect");
+
+const Person = db.define("person", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: Sequelize.STRING(2),
+    allowNull: false,
+  },
+  city: {
+    type: Sequelize.STRING(2),
+    allowNull: false,
+  },
+  age: {
+    type: Sequelize.NUMBER(3),
+    allowNull: false,
+  },
+  modality: {
+    type: Sequelize.STRING(9),
+    allowNull: false,
+  },
+  fish_weight: {
+    type: Sequelize.NUMBER(3),
+  },
+  fish_type: {
+    type: Sequelize.STRING,
+  },
+});
