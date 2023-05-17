5e06eec84b9edc056bc71305d2bdb792f89412d1
Guilherme-Linux <gui.rock08@gmail.com>
2023-05-16 21:27:13 -0300

    Update

--- controller/fisherController.js
+++ controller/fisherController.js
@@ -25,7 +25,7 @@ exports.save = async (req, res) => {
   }).then((post) => res.send(post));
 };
 
-// function to filter users
+// Function to filter users
 exports.getById = async (req, res) => {
   const { id } = req.params;
 
@@ -40,4 +40,51 @@ exports.getById = async (req, res) => {
     res.status(500).send({ message: 'Internal Server Error' });
   }
 };
+
+// Function to delete a user
+exports.delete = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const person = await Person.findByPk(id);
+    if (!person) {
+      return res.status(404).send({ message: 'Person not found' });
+    }
+    await person.destroy();
+    res.send({ message: 'Person deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal Server Error' });
+  }
+};
+
+// Function to update a user
+exports.update = async (req, res) => {
+  const { id } = req.params;
+  const { name, state, city, age, modality, fish_weight, fish_type } = req.body;
+
+  try {
+    let person = await Person.findByPk(id);
+    if (!person) {
+      return res.status(404).send({ message: 'Person not found' });
+    }
+
+    person.name = name;
+    person.state = state;
+    person.city = city;
+    person.age = age;
+    person.modality = modality;
+    person.fish_weight = fish_weight;
+    person.fish_type = fish_type;
+
+    await person.save();
+
+    res.send(person);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal Server Error' });
+  }
+};
+
+
 
--- router/routes.js
+++ router/routes.js
@@ -8,13 +8,13 @@ const fisherController = require("../controller/fisherController");
 router
   .route("/user")
   .post((req, res) => fisherController.save(req, res))
-  .get((req, res) => fisherController.list(req, res))
-  .put((req, res) => fisherController.save(req, res))
-  .delete((req, res) => fisherController.delete(req, res));
+  .get((req, res) => fisherController.list(req, res));
 
 // Route to get a user by ID
 router
   .route("/user/:id")
-  .get((req, res) => fisherController.getById(req, res));
+  .get((req, res) => fisherController.getById(req, res))
+  .put((req, res) => fisherController.update(req, res))
+  .delete((req, res) => fisherController.delete(req, res));
 
 module.exports = router;
